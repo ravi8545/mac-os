3c93069390b0e27610b8efb6674116b0f89cab44
ravi8545 <139250843+ravi8545@users.noreply.github.com>
2026-04-26 12:48:07 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './app.scss'
+import Doc from './components/Dock'
+
+
+
+
+
+
+const App = () => {
+  return (
+    <main>
+      <Doc/>
+    </main>
+  )
+}
+
+export default App
